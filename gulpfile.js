63864c5093adb19ddcf66e5ea5927cb720cdbbd2
YeLinli <yelinli@corp.netease.com>
2018-08-16 11:04:49 +0800

    add:gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const minifyCss = require('gulp-minify-css');
+const minifyImg = require('gulp-imagemin');
+const uglifyes = require('gulp-uglifyes');
+gulp.task('css',()=>{
+    gulp.src('./public/css/*.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./public/css'));
+});
+gulp.task('js',()=>{
+    gulp.src('./public/lib/fancybox/*.js')
+    .pipe(uglifyes({
+        ecma:5,
+        warnings:true
+    }))
+    .pipe(gulp.dest('./public/lib/fancybox'));
+})
+gulp.task('image',()=>{
+    gulp.src('./public/images/*')
+    .pipe(minifyImg([
+        minifyImg.jpegtran({progressive:true}),
+    ]))
+    .pipe(gulp.dest('./public/images'));
+
+});
+gulp.task('default',['css','js','image']);
