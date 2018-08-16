8e7d292cdf3f5eec109c264f83164b48594cdcc0
YeLinli <yelinli@corp.netease.com>
2018-08-16 12:20:11 +0800

    modify:gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const gulp = require('gulp');
 const minifyCss = require('gulp-minify-css');
 const minifyImg = require('gulp-imagemin');
@@ -25,9 +24,3 @@ gulp.task('image',()=>{
 
 });
 gulp.task('default',['css','js','image']);
-=======
-var gulp = require('gulp');
-gulp.task('default',function(){
-
-});
->>>>>>> bcc078cd7e0f4c6215b4a0faff96357686af3350
